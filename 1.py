c17c90e3293bb392afe3b081e0c7986edc0af472
totymachev1980@mail.ru <Njnsvfxtd2>
2022-09-01 11:22:44 +0700

    Коммит 3

--- 1.py
+++ 1.py
@@ -1,10 +1,13 @@
 import datetime
 
-name = input('Укажите своё имя: ')
-print(f'Привет, {name}!')
+#name = input('Укажите своё имя: ')
+print(f'Привет, Евгений!')
 
 a, b = 2, 5
 print(a + b)
 
-t = datetime.time.hour
+t = datetime.datetime.now()
 print(t)
+
+t1 = datetime.MINYEAR
+print(t1)
